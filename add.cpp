a971504efe56ec0dc1945e00b00352ecb92e53d5
COLDY~ <gboro2004@gmail.com>
2024-03-26 11:38:02 +0530

    initial commit

---
+++ add.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n1,n2,result;
+    cout<<"Enter two numbers : "<< endl;
+    cin>> n1 >> n2;
+    result = n1 + n2;
+    cout<<"Result = "<< result;
+}
